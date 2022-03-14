744c5e3480ff7fd9c1f305b42ef257487ade0a02
Anselem <anselemodims@gmail.com>
2022-03-14 17:05:19 +0100

    Test: write tests for the can_be_archived method of the Book class

--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -10,4 +10,25 @@ describe 'Book' do
       expect(@book).to be_instance_of Book
     end
   end
+
+  context 'instance methods' do
+    it 'should return true if published_date is older than 10 years' do
+      expect(@book.can_be_archived?).to eq true
+    end
+
+    it 'should return true if published_date is older than 10 years' do
+    @book = Book.new('John', 'bad', 20)
+      expect(@book.can_be_archived?).to eq true
+    end
+
+    it 'should return true if cover_state equals to "bad"' do
+      @book = Book.new('John', 'bad', 9)
+        expect(@book.can_be_archived?).to eq true
+    end
+
+    it 'should return false if cover_state equals to "bad"' do
+      @book = Book.new('John', 'good', 9)
+        expect(@book.can_be_archived?).to eq false
+    end
+  end
 end
