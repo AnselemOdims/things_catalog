cc6a18d8f8bdd9ef99caa720b0f6823d823a716d
Ahmed <ahmedreda152@gmail.com>
2022-03-15 18:14:52 +0200

    fix primary key dublication

--- Schema/game_author_schema.sql
+++ Schema/game_author_schema.sql
@@ -1,20 +1,22 @@
 CREATE TABLE items(
     id SERIAL PRIMARY KEY,
     archived BOOL DEFAULT 'f',
-    author_id SERIAL PRIMARY KEY,
+    author_id SERIAL NOT NULL,
     publish_date TIMESTAMP NOT NULL,
     CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES Author(id),
-)
- CREATE TABLE author(
+);
+
+CREATE TABLE author(
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     items TEXT []
-)
- CREATE TABLE game(
+);
+
+CREATE TABLE game(
     id SERIAL PRIMARY KEY,
     multiplayer VARCHAR(50),
     last_played TIMESTAMP NOT NULL,
-    item_id SERIAL PRIMARY KEY,
+    item_id SERIAL NOT NULL,
     CONSTRAINT item_fk FOREIGN KEY (item_id) REFERENCES items(id)
-)
+);
