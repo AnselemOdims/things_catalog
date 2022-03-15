6d0b838f94c2799ec52e70056e62bbcb56c71ea9
Anselem <anselemodims@gmail.com>
2022-03-15 09:29:52 +0100

    Feat: define a welcome and list of options methods for user prompt

---
+++ main.rb
@@ -0,0 +1,17 @@
+def welcome
+  puts '*** Welcome to the thingsCatalog App ***'
+end
+
+def list_of_options
+  puts ['1: List all books 📖',
+        '2: List all music albums',
+        '3: List all games',
+        '4: List all genres (e.g \'Comedy\', \'Thriller\')',
+        '5: List all labels (e.g. \'Gift\', \'New\')',
+        '6: List all authors (e.g. \'Stephen King\')',
+        '7: Add a book 📖',
+        '8: Add a music album',
+        '9: Add a game',
+        '10: Exit and save'
+      ]
+end
