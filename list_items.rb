0850248b89ded17cd0b963b240f84e58f831f4ac
Anselem <anselemodims@gmail.com>
2022-03-15 11:47:44 +0100

    Feat: create a method to list all books

---
+++ list_items.rb
@@ -0,0 +1,14 @@
+require 'json'
+
+class List
+  def self.list_books
+    books = JSON.parse(File.read('books.json'))
+    if books.empty?
+      "No books added yet"
+    else
+      books.each_with_index do |book, index|
+        puts "#{index+1})Publisher: #{book['publisher']}, Cover State: #{book['cover_state']}, Published Date: #{book['publish_date']}"
+      end
+    end
+  end
+end
